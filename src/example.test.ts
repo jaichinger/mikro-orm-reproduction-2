04068eb14e08294ad2aa07ffd5245f812482062e
James Aichinger <james.aichinger@gmail.com>
2025-08-29 15:16:51 +1000

    added composite key test case

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,51 +1,178 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import {
+  Entity,
+  ManyToOne,
+  MikroORM,
+  PrimaryKey,
+  Property,
+  ref,
+  Ref,
+} from "@mikro-orm/sqlite";
+
+@Entity()
+class Organisation {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+}
 
 @Entity()
 class User {
+  @ManyToOne({
+    entity: () => Organisation,
+    fieldName: "org_id",
+    primary: true,
+    ref: true,
+  })
+  org!: Ref<Organisation>;
 
   @PrimaryKey()
   id!: number;
 
   @Property()
-  name: string;
+  name!: string;
 
-  @Property({ unique: true })
-  email: string;
+  @ManyToOne({
+    entity: () => Workspace,
+    ref: true,
+    fieldNames: ["org_id", "workspace_id"],
+    ownColumns: ["workspace_id"],
+  })
+  workspace!: Ref<Workspace>;
+}
 
-  constructor(name: string, email: string) {
-    this.name = name;
-    this.email = email;
-  }
+@Entity()
+class Workspace {
+  @ManyToOne({
+    entity: () => Organisation,
+    fieldName: "org_id",
+    primary: true,
+    ref: true,
+  })
+  org!: Ref<Organisation>;
 
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+}
+
+@Entity()
+class UserRequest {
+  @ManyToOne({
+    entity: () => Organisation,
+    fieldName: "org_id",
+    primary: true,
+    ref: true,
+  })
+  org!: Ref<Organisation>;
+
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+
+  @ManyToOne({
+    entity: () => User,
+    ref: true,
+    fieldNames: ["org_id", "user_id"],
+    ownColumns: ["user_id"],
+  })
+  user!: Ref<User>;
 }
 
 let orm: MikroORM;
 
 beforeAll(async () => {
   orm = await MikroORM.init({
-    dbName: ':memory:',
-    entities: [User],
-    debug: ['query', 'query-params'],
+    dbName: ":memory:",
+    entities: [Organisation, User, Workspace, UserRequest],
+    debug: ["query", "query-params"],
     allowGlobalContext: true, // only for testing
   });
-  await orm.schema.refreshDatabase();
+
+  await orm.schema.refreshDatabase({ dropDb: true });
+
+  const org = orm.em.create(Organisation, { id: 1, name: "org1" });
+
+  const ws = orm.em.create(Workspace, {
+    org: org,
+    id: 1,
+    name: "ws1",
+  });
+
+  const user = orm.em.create(User, {
+    org: org,
+    id: 1,
+    name: "user1",
+    workspace: ws,
+  });
+
+  orm.em.create(UserRequest, {
+    org: org,
+    id: 1,
+    name: "userRequest1",
+    user: user,
+  });
+
+  await orm.em.flush();
+  orm.em.clear();
 });
 
 afterAll(async () => {
   await orm.close(true);
 });
 
-test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
-  await orm.em.flush();
-  orm.em.clear();
+test("composite foreign key as array", async () => {
+  const requests = await orm.em.find(
+    UserRequest,
+    {
+      user: {
+        workspace: [1, 1],
+      },
+    },
+    {
+      populate: ["user"],
+    }
+  );
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
-  await orm.em.flush();
+  expect(requests).toHaveLength(1);
+  expect(requests[0].name).toBe("userRequest1");
+});
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+test("composite foreign key as ref", async () => {
+  const requests = await orm.em.find(
+    UserRequest,
+    {
+      user: {
+        workspace: ref(Workspace, [1, 1]),
+      },
+    },
+    {
+      populate: ["user"],
+    }
+  );
+
+  expect(requests).toHaveLength(1);
+  expect(requests[0].name).toBe("userRequest1");
+});
+
+test("composite foreign key as object", async () => {
+  const requests = await orm.em.find(
+    UserRequest,
+    {
+      user: {
+        workspace: { org: 1, id: 1 },
+      },
+    },
+    {
+      populate: ["user"],
+    }
+  );
+
+  expect(requests).toHaveLength(1);
+  expect(requests[0].name).toBe("userRequest1");
 });
